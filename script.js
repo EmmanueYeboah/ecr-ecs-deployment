b8a2a5b41d01122d3e2ceeb1e9367a643101f686
EmmanueYeboah <nanayeb87@gmail.com>
2025-07-06 22:30:51 +0000

    ecr-ecs deployment

--- script.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", function () {
-    const form = document.getElementById("contact-form");
-    form.addEventListener("submit", function (e) {
-        e.preventDefault();
-        alert("Thank you! We will get back to you soon.");
-    });
-});
